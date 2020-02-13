3a7adf9fced719300c77197180f4720b7726cd2c
HillmerCh <hilac@msn.com>
2020-02-12 20:09:50 -0500

    Update BasicCalculator.java

--- src/main/java/org/medellin/app/calculator/BasicCalculator.java
+++ src/main/java/org/medellin/app/calculator/BasicCalculator.java
@@ -9,7 +9,7 @@ public class BasicCalculator {
 
     public Long sum(Long number1, Long number2) {
         logger.info( "Summing {} + {}", number1, number2 );
-        return number1 + number2;
+        return number1 - number2;
     }
 
 }
